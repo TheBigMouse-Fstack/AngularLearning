525478d6147b83fd8b41503207c8f96fba6e1390
TheBigMouse-MernStack <khanhnqse19@gmail.com>
2025-05-30 10:27:26 +0700

    OutputtingTask

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -35,7 +35,7 @@ export class TasksComponent {
       dueDate: '2024-06-15',
     },
   ];
-  get selectedUserTask() {
+  get selectedUserTasks() {
     return this.tasks.filter((task) => task.userId === this.userId);
   }
 }
